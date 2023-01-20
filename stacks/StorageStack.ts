d6421b41e4fa2d078d519d57ec52b9506926e9dc
Jackson Hammond <malts18@gmail.com>
2023-01-20 18:03:37 -0500

    app permissive cors to s3 bucket

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -2,7 +2,16 @@ import { Bucket, StackContext } from "@serverless-stack/resources";
 import { Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }: StackContext) {
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
 
   const table = new Table(stack, "Notes", {
     fields: {
