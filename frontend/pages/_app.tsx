7f9b52923968435545cbef442d8255cc6d342b52
Jackson Hammond <malts18@gmail.com>
2023-01-23 12:32:15 -0500

    simple form to POST from next and create resource through API gateway

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import config from "../amplify.config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  Amplify.configure({
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+    },
+    API: {
+      endpoints: [
+        {
+          name: "notes",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION,
+        },
+      ],
+    },
+  });
+  return <Component {...pageProps} />;
 }
