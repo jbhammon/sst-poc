1528e56389bb008d2496fe4bfeaffa122e456f04
Jackson Hammond <malts18@gmail.com>
2023-01-23 14:40:22 -0500

    Clean up homepage with tailwind

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
@@ -11,12 +10,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>Hello world</h1>
-        <div className="bg-purple-400">
-          <p>Quick Tailwind example</p>
+      <main>
+        <div className="container mx-auto mt-8 px-8 flex flex-col space-y-6">
+          <h1 className="text-2xl">Simple Notes 🍩</h1>
+          <p>A simple serverless app made with SST</p>
+          <Link href="/create-note">
+            <a className="text-purple-500 underline decoration-purple-500">
+              Create new note
+            </a>
+          </Link>
         </div>
-        <Link href="/create-note">Create a new note</Link>
       </main>
     </>
   );
